5a251b8c144f9297945c0e2d0f2d54f039b79207
dorvillethiago <dorville.dev@gmail.com>
2023-12-27 13:36:33 -0300

    first commit

---
+++ face_checker.py
@@ -0,0 +1,65 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import base64
+import numpy as np
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+def get_direction(base64):
+    image_array = np.frombuffer(base64, dtype=np.uint8)
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = face_mesh.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    img_h, img_w, img_c = image.shape
+    face_3d = []
+    face_2d = []
+
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            for idx, lm in enumerate(face_landmarks.landmark):
+                if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
+
+                    x, y = int(lm.x * img_w), int(lm.y * img_h)
+
+                    face_2d.append([x, y])
+
+                    face_3d.append([x, y, lm.z])
+            
+            face_2d = np.array(face_2d, dtype=np.float64)
+
+            face_3d = np.array(face_3d, dtype=np.float64)
+
+            focal_length = 1 * img_w
+
+            cam_matrix = np.array([ [focal_length, 0, img_h / 2],
+                                    [0, focal_length, img_w / 2],
+                                    [0, 0, 1]])
+            dist_matrix = np.zeros((4, 1), dtype=np.float64)
+
+            success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
+
+            rmat, jac = cv2.Rodrigues(rot_vec)
+
+            angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
+
+            x = angles[0] * 360
+            y = angles[1] * 360
+
+            if y < -10:
+                text = "Left"
+            elif y > 10:
+                text = "Right"
+            elif x < -10:
+                text = "Down"
+            elif x > 10:
+                text = "Up"
+            else:
+                text = "Forward"
+            return text
+    
---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import WebSocket
+from fastapi import FastAPI
+from face_checker import get_direction
+
+app = FastAPI()
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message, websocket):
+        await websocket.send_text(message)
+
+manager = ConnectionManager()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    while True:
+        data = await websocket.receive_bytes()
+        direction = get_direction(data)
+        await manager.send_message(direction, websocket)
